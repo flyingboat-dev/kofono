69e87456f75887f7c0080f3058b7f3c67477bdb6
1franck <1209308+1franck@users.noreply.github.com>
2026-04-17 07:21:39 -0400

    update to v4

--- packages/kofono-solid-form/tailwind.config.ts
+++ packages/kofono-solid-form/tailwind.config.ts
@@ -1,7 +1,5 @@
 /** @type {import("tailwindcss").Config} */
-module.exports = {
+export default {
     darkMode: "selector",
     content: ["./dev/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-    plugins: [require("tailwindcss-animate")],
-    presets: [],
 };
